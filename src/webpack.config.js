cd7cd71b78feb5d92bb8cb4cd35488bf54feb32f
Karan Luciano <karan.pernis@cassi.com.br>
2023-04-28 08:10:44 -0400

    ajuste

---
+++ src/webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    entry: './src/index.js', // Caminho do arquivo JavaScript principal da sua aplicação
+    output: {
+      filename: 'bundle.js', // Nome do arquivo de saída
+    },
+    resolve: {
+      extensions: ['.js'],
+    },
+  };
+  
